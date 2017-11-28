581c20245f7b12189b0ceddb3cdd2f120fc78ebf
dave <dave@vsni.co.uk>
2017-11-28 14:05:32 +0800

    Upload for the first time.

---
+++ R/vsnc.R
@@ -0,0 +1,8 @@
+#' A package that provide data and analysis code in Agri
+#' 
+#' That contains plant breeding, forest breeding, animal breeding etc.
+#' It also have some functions that assist asreml package.
+#' 
+#' @author Dave<Dave@vsni.co.uk>
+#' @name vsnc
+NULL
